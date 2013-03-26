9870a9f206fcd1a72b829d32840568fa36f1008a
Himansu Desai <himansudesai@Himansus-MacBook-Pro.local>
2013-03-25 17:22:45 -0700

    Full blown enyo app

---
+++ synergy-demo-2/source/RandomNumber.js
@@ -0,0 +1,5 @@
+function RandomNumber() {
+}
+RandomNumber.next = function() {
+    return (Math.floor(Math.random()*6));
+}
---
+++ synergy-demo-2/source/package.js
@@ -0,0 +1,7 @@
+enyo.depends(
+    "../lib/onyx",
+    "../lib/layout",
+    "RandomNumber.js",
+    "App.js",
+    "App.css"
+);
