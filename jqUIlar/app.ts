322407aa2ad9bc9f43bbb29ddd9dc123d798cb81
Himansu Desai <himansudesai@gmail.com>
2016-01-17 21:12:30 -0800

    include tiny code snippets along with the demo, for how to use each wrapper component

--- jqUIlar/app.ts
+++ jqUIlar/app.ts
@@ -11,9 +11,9 @@ import { jqUIlarSlider, jqUIlarDatePicker, jqUIlarProgressBar } from './jquilar.
 @View({
   template: `
     <br/>
-    <h2 class="section-header">
-      Slider
-    </h2>
+    <div class="section-header">
+      <span>Slider</span><p class="code-snippet">\n&lt;jquilar-slider [valueObj]=&quot;some_val&quot; (stop)=&quot;func($event)&quot;&gt;&lt;/jquilar-slider&gt;/jquilar-slider&gt;</p>
+    </div>
     <div class="muted big">Slider object value: {{sliderVal}}</div>
     <jquilar-slider id="slider1" [value]="sliderVal" [step]="2" (stop)="sliderStopped($event)"></jquilar-slider><br/>
     <jquilar-slider id="slider2" [orientation]="'vertical'" [value]="sliderVal" (stop)="sliderStopped($event)"></jquilar-slider><br/>
@@ -21,20 +21,20 @@ import { jqUIlarSlider, jqUIlarDatePicker, jqUIlarProgressBar } from './jquilar.
     <br/>
     <button class="whiteonslategrey" role="button" (click)="resetSlider()">Reset slider object</button><br/><br/>
     <h2 class="section-header">
-      Date Picker
+      <span>Date Picker</span><p class="code-snippet">&lt;jquilar-datepicker [value]=&quot;dateObj&quot; (select)=&quot;func($event)&quot;&gt;&lt;/jquilar-datepicker&gt;</p>
     </h2>
     <jquilar-datepicker [value]="dateVal" [changeMonth]="true" [changeYear]="true" (select)="dateSelected($event)" class="gold"></jquilar-datepicker>
     <jquilar-datepicker [value]="dateVal" (select)="dateSelected($event)" class="gold"></jquilar-datepicker><br/><br/>
     <button class="whiteonslategrey" role="button" (click)="resetDate()">Reset date object</button><br/><br/>
     <h2 class="section-header">
-      Progress Bar
+      <span>Progress Bar</span><p class="code-snippet">&lt;jquilar-progressbar [value]=&quot;progressVal&quot; (change)=&quot;progressChanged($event)&quot; class=&quot;gold&quot;&gt;&lt;/jquilar-progressbar&gt;</p>
     </h2>
     <div class="muted big">Current value: {{progressVal}}</div>
     <jquilar-progressbar [value]="progressVal" (change)="progressChanged($event)" class="gold"></jquilar-progressbar><br/>
     <jquilar-progressbar [value]="progressVal" (select)="progressChanged($event)" class="gold"></jquilar-progressbar><br/>
-    <button class="whiteonslategrey" role="button" (click)="decProgressBar()">Dec</button>
+    <button class="whiteonslategrey" role="button" (click)="decProgressBar()">-10</button>
     <button class="whiteonslategrey" role="button" (click)="resetProgressBar()">Reset Progress Object</button>
-    <button class="whiteonslategrey" role="button" (click)="incProgressBar()">Inc</button><br/><br/>
+    <button class="whiteonslategrey" role="button" (click)="incProgressBar()">+10</button><br/><br/>
   `,
   directives: [jqUIlarSlider, jqUIlarDatePicker, jqUIlarProgressBar]
 })
